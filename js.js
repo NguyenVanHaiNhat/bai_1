fe43a8e476369dfae749f16d6c5b653bf77bf426
nhat0120230 <nhatnguyen221020032@gmail.com>
2023-11-13 10:33:46 +0700

    thuc hanh

---
+++ js.js
@@ -0,0 +1,13 @@
+// let a = +document.getElementById('c').value;
+
+function changeCtoF() {
+    let a = +document.getElementById('c').value;
+    let y = a * 9/5 + 32;
+    document.getElementById('result').innerHTML = 'result :' + y + ' độ C';
+}
+
+function changeFtoC() {
+    let a = +document.getElementById('c').value;
+    let z = a * 5/9 - 32;
+    document.getElementById('result').innerHTML ='result :' + z + ' độ F';
+}
